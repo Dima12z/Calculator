60f431c34d86c44338a5b6f1d4713142a2f6f895
Dima12z <99693184+Dima12z@users.noreply.github.com>
2024-10-19 23:18:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Calculator
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter operator (+, -, *, /): ");
+        string opre = Console.ReadLine();
+
+        double result = 0;
+
+        switch (opre)
+        {
+            case "+": result = num1 + num2; break;
+            case "-": result = num1 - num2; break;
+            case "*": result = num1 * num2; break;
+            case "/": result = (double)num1 / num2; break;
+            default: Console.WriteLine("Invalid operator"); return;
+        }
+
+        Console.WriteLine("Result: " + num1 +" "+ opre + " "+num2 + " = " + result); 
+    }
+}//Dima's code 
